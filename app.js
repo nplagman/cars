16204d3065b695081a7572b3090032d2bfcd779c
Nick Plagman <nrplagma@gmail.com>
2016-11-30 16:23:41 -0500

    step 5

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var cars = require('./routes/cars');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layout');   // defaults to 'layout'
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,13 +24,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressLayouts);
+app.use(methodOverride('_method'));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/cars', cars);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Haha you suck');
   err.status = 404;
   next(err);
 });
@@ -44,3 +49,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+console.log("The server is listening!")
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) {  //function is a controller function, it's what we want to call when we hit the route
+  res.render('index', {
+    title: 'Express', }); //Express is the content that index
 });
 
+
+
+
+router.get('/about', function(req,res, next) {
+    res.render('about', {
+      title: 'Cars',
+      technologies: ['Express', 'EJS', 'Bootstrap']
+
+    });
+});
 module.exports = router;
